57c5a5543e9c6c06a1d8a798740db0476af7bd21
Tasdyque <tasdyque.syed@gmail.com>
2022-09-29 20:01:33 -0400

    added http request functionality and backend connection

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
 import "./index.css";
+import { AuthProvider } from "./Components/backendConnection/AuthProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
