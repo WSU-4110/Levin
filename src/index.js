06a24010abf85bc6da19016a957a860bcc66696b
Logan Barnes <gw4822@wayne.edu>
2022-10-13 18:40:32 -0400

    Added back the orignal reactDOM render under that is the commented out version of the reactDOM render for the test files

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import App from "./App.js";
 import { AuthProvider } from "./Backend/AuthProvider";
 import Board from "./Components/testBoard.js";
 
-
 ReactDOM.render(
-  <Board knightPosition={[0,0]} />,
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
+
+// ReactDOM.render(
+//  <Board knightPosition={[0,0]} />,
+//  document.getElementById("root")
+// );
