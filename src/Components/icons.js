8e1a45f0a6cd0f859d50feb7d1b2faa1ffb8b86e
Angjelo154 <70215996+Angjelo154@users.noreply.github.com>
2022-11-04 19:05:04 -0400

    Singleton Design Pattern

--- src/Components/icons.js
+++ src/Components/icons.js
@@ -1,5 +1,14 @@
 import React from "react";
 import { motion } from "framer-motion";
+import TableChartIcon from "@mui/icons-material/TableChart";
+import SupportSharpIcon from "@mui/icons-material/SupportSharp";
+import SettingsIcon from "@mui/icons-material/Settings";
+import LoginSharpIcon from "@mui/icons-material/LoginSharp";
+import LogoutIcon from "@mui/icons-material/Logout";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import PrivacyTipIcon from "@mui/icons-material/PrivacyTip";
+import HomeRepairServiceIcon from "@mui/icons-material/HomeRepairService";
+import CopyrightIcon from "@mui/icons-material/Copyright";
 import "./Styling/icons.css";
 
 function Icons({ icon }) {
@@ -16,6 +25,14 @@ function Icons({ icon }) {
         }}
       >
         {icon}
+      <Icons>
+        <Icons icon={<TableChartIcon />} />
+        <Icons icon={<SupportSharpIcon />} />
+        <Icons icon={<LoginSharpIcon />} />
+        <Icons icon={<HelpOutlineIcon />} />
+        <Icons icon={<HomeRepairServiceIcon />} />
+        <Icons icon={<CopyrightIcon />} />
+        </Icons>
       </motion.div>
     </motion.div>
   );
