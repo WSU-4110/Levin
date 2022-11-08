a8c48cee8d3e31e11281c4d4f5c55e4fdbad515e
Tahmid Faiz <ftahmid2546@gmail.com>
2022-11-07 23:44:34 -0500

    adding tutorialModal.js
    git status

--- src/Components/tutorialModal.js
+++ src/Components/tutorialModal.js
@@ -5,6 +5,32 @@ import React from "react";
 // Temp Video Example
 import Video from "./Tutorial_Video/Temp_Video.mp4";
 
+
+var Singleton = (function () {
+    var instance;
+
+    function createInstance() {
+        var object = new Object("I am the instance");
+        return object;
+    }
+
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
+
+function run() {
+
+    var instance1 = Singleton.getInstance();
+    var instance2 = Singleton.getInstance();
+
+    console.log("Same instance? " + (instance1 === instance2));
+}
 const dropIn = {
   hidden: {
     y: "-100vh",
