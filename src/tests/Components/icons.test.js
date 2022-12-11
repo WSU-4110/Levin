e9053b7e6a989b97474e7457c91499e86e9868f4
Tahmid Faiz <ftahmid2546@gmail.com>
2022-12-10 19:20:03 -0500

    #reset

--- src/tests/Components/icons.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Icons from "../../Components/icons";
-
-test("Should render animation", () => {
-  render(<Icons />);
-  const I1 = screen.getByTestId("I1");
-  expect(I1).toBeInTheDocument();
-});
